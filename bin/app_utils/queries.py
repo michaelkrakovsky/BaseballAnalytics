f0e05f1aa104d0e321ea82586152edbda339f97a
michaelkrakovsky <34549116+michaelkrakovsky@users.noreply.github.com>
2020-05-30 23:39:12 -0400

    In Progress: Finishing pipeline to add features for a game

--- bin/app_utils/queries.py
+++ bin/app_utils/queries.py
@@ -84,27 +84,104 @@ class Queries():
         #    the starting lineup.
         # Function Parameters: game_id (The game_id you wish to use to get the game participants.)
         # Function Throws: Nothing
-        # Function Returns: A tuple of two lists. The first list contains the home team names while the second list contains the away teams.
+        # Function Returns: A tuple containing two lists. The first list contains the home team names while the second list contains the away teams.
 
         # Home Team equals 1 for Batting Team. The query is formatted like such: 
         #     Game_ID, Batting_Team, Pitcher_Name, Catcher, Center_Field, Left_Field, Right_Field, First_Base, Second_Base, Third_Base, Shortstop
-        game_participants = """
-                            select Distinct game_day.Game_ID, batter_in_event.Batting_Team, pitcher_in_event.Pitcher_Name,       
-                            event_catcher.Catcher, event_centre_field.Center_Field, event_left_field.Left_Field,
-                            event_right_field.Right_Field, event_first_base.First_Base, event_second_base.Second_Base, 
-                            event_third_base.Third_Base, event_shortstop.Shortstop
-                                from game_day
-                                inner join event_instance on game_day.Game_ID=event_instance.Game_ID
-                                inner join batter_in_event on event_instance.idEvent=batter_in_event.idEvent
-                                inner join pitcher_in_event on pitcher_in_event.idEvent=event_instance.idEvent
-                                inner join event_catcher on event_catcher.idEvent=event_instance.idEvent
-                                inner join event_centre_field on event_centre_field.idEvent=event_instance.idEvent
-                                inner join event_left_field on event_left_field.idEvent=event_instance.idEvent
-                                inner join event_right_field on event_right_field.idEvent=event_instance.idEvent
-                                inner join event_first_base on event_first_base.idEvent=event_instance.idEvent
-                                inner join event_second_base on event_second_base.idEvent=event_instance.idEvent
-                                inner join event_third_base on event_third_base.idEvent=event_instance.idEvent
-                                inner join event_shortstop on event_shortstop.idEvent=event_instance.idEvent
-                                where event_instance.Game_ID = '{}' order by batter_in_event.batting_team, CHAR_LENGTH(event_instance.idEvent), event_instance.idEvent;
-                                """.format(game_id)
-        game_vistors = game_participants[0][2:]             # Given the query conditions, this will always provide the starting lineups.
+        game_participants = self.fetch_data("""
+                                            select Distinct game_day.Game_ID, batter_in_event.Batting_Team, pitcher_in_event.Pitcher_Name,       
+                                            event_catcher.Catcher, event_centre_field.Center_Field, event_left_field.Left_Field,
+                                            event_right_field.Right_Field, event_first_base.First_Base, event_second_base.Second_Base, 
+                                            event_third_base.Third_Base, event_shortstop.Shortstop
+                                                from game_day
+                                                inner join event_instance on game_day.Game_ID=event_instance.Game_ID
+                                                inner join batter_in_event on event_instance.idEvent=batter_in_event.idEvent
+                                                inner join pitcher_in_event on pitcher_in_event.idEvent=event_instance.idEvent
+                                                inner join event_catcher on event_catcher.idEvent=event_instance.idEvent
+                                                inner join event_centre_field on event_centre_field.idEvent=event_instance.idEvent
+                                                inner join event_left_field on event_left_field.idEvent=event_instance.idEvent
+                                                inner join event_right_field on event_right_field.idEvent=event_instance.idEvent
+                                                inner join event_first_base on event_first_base.idEvent=event_instance.idEvent
+                                                inner join event_second_base on event_second_base.idEvent=event_instance.idEvent
+                                                inner join event_third_base on event_third_base.idEvent=event_instance.idEvent
+                                                inner join event_shortstop on event_shortstop.idEvent=event_instance.idEvent
+                                                where event_instance.Game_ID = '{}' order by batter_in_event.batting_team, CHAR_LENGTH(event_instance.idEvent), event_instance.idEvent;
+                                                """.format(game_id))
+        game_vistors = game_participants[0][2:]                             # Given the query conditions, this will always provide the starting lineups.
+        for loc, part in enumerate(game_participants):
+            if part[1] == 1:
+                game_homers = game_participants[loc][2:]                      # This will provide the values of the home team. Once retrieved, we have all what we need.
+                break
+        return (game_vistors, game_homers)
+
+    def get_offensive_features(self, player_id, game_id):
+
+        # Function Description: Retrieve the features of a given player prior to entering the new game. The features are available from the previous game.
+        # Function Parameters: player_id (The player id associated in which we wish to retrieve the data.), game_id (The game id needed to look backwards.)
+        # Function Throws: Nothing
+        # Function Returns: A list containing the offensive features. The amount of features was determined in previous queries but does not matter in this function.
+        #    If the player does not have much data, I will be returning -1 to signal the prescence of a new player.
+
+        features = self.fetch_data("""
+                                    select Ten_Rolling_BA, Ten_Rolling_OBP, Ten_Rolling_SLG from offensive_features inner join
+                                    game_day on game_day.Game_ID=offensive_features.Game_ID
+                                    where player_id = '{}'
+                                    and game_day.Date < (select game_day.Date from game_day where game_day.Game_ID = '{}')
+                                    order by game_day.Date Desc;
+                                    """.format(player_id, game_id))
+        if len(features) < 10:
+            return [-1, -1, -1]
+        return list(features[0])                           # Return the first row which contains the data from the previous day.
+
+    def get_pitchers_features(self, player_id, game_id):
+
+        # Function Description: Retrieve the features of a given pitcher prior to entering the new game. The features are available from the previous game.
+        # Function Parameters: player_id (The player id associated in which we wish to retrieve the data.), game_id (The game id needed to look backwards.)
+        # Function Throws: Nothing
+        # Function Returns: A list containing the offensive features. The amount of features was determined in previous queries but does not matter in this function.
+        #    If the player does not have much data, I will be returning -1 to signal the prescence of a new player.
+
+        features = self.fetch_data("""
+                                    select Ten_Rolling_Ks, Ten_Rolling_WHIP, Ten_Rolling_RA from pitching_features inner join
+                                    game_day on game_day.Game_ID=pitching_features.Game_ID
+                                    where player_id = '{}'
+                                    and game_day.Date < (select game_day.Date from game_day where game_day.Game_ID = '{}')
+                                    order by game_day.Date Desc;
+                                    """.format(player_id, game_id))
+        if len(features) < 5:                              # I reduced the number of previous games for pitcher by half.
+            return [-1, -1, -1]
+        return list(features[0])                           # Return the first row which contains the data from the previous day.
+
+    def sub_pitching_features(self, pitchers, game_id):
+
+        # Function Description: Substitute the pitcher ids provided with the pitching features. The features will be returned in the same order the names are provided.
+        # Function Parameters: pitchers (The player id we wish to retrieve the data for.), game_id (The game id needed to look backwards.) 
+        # Function Throws: Nothing
+        # Function Returns: A complete list of the featues to be inputted into the model. The list will vary depending on the number of players and features for each player.
+
+        pitcher_features = []
+        for pitcher in pitchers:
+            pitcher_features = pitcher_features + self.get_pitchers_features(pitcher, game_id)
+        return pitcher_features
+
+    def sub_offensive_features(self, batters, game_id):
+
+        # Function Description: Substitute the batters ids provided with the batting features. The features will be returned in the same order the names are provided.
+        # Function Parameters: batters (The player id we wish to retrieve the data for.), game_id (The game id needed to look backwards.) 
+        # Function Throws: Nothing
+        # Function Returns: A complete list of the featues to be inputted into the model. The list will vary depending on the number of players and features for each player.
+
+        offensive_features = []
+        for batter in batters:
+            offensive_features = offensive_features + self.get_offensive_features(batter, game_id)
+        return offensive_features
+
+    def get_game_features(self, game_id):
+
+        # Function Description: Get all the features for a given game id. This involves getting the players who played in the game and then retrieving their associated features.
+        # Function Parameters: game_id (The game id used to acquire the player features.)
+        # Function Throws: Nothing
+        # Function Returns: A single list containing the features of the game.
+
+        game_features = []
+        vis_players, home_players = self.get_players_in_game_vOne(game_id)
--- bin/db/architecture/playground.sql
+++ bin/db/architecture/playground.sql
@@ -7,48 +7,19 @@
 #select * from positional_player_details;
 #INSERT IGNORE INTO positional_player_details (player_id, Last_Name, First_Name, Player_Debut) values ('michael', 'deleteMe', 'deleteMe', '2019-04-23');
 
-# --------------------------------------- Respective to Game Day ---------------------------------------- #
-#select * from game_day;
-#INSERT IGNORE INTO game_day (Visiting_Team, Home_Team, Date, Game_ID, NumGameInDay) Values ('SEA', 'TEX', '2015-12-22', 'PHI199209271', '1');
-# --------------------------------------- Respective to Event ---------------------------------------- #
-#select * from event_instance
-#INSERT INTO event_instance (idEvent, Game_ID, Inning, Batting_Team, Outs, Balls, Strikes, Pitch_Sequence, Vis_Score, Home_Score, Batter_Name, Batter_Hand, Pitcher_Name, Pitcher_Hand, Event_Text, Leadoff_Flag, Pinch_Hit_Flag, Defensive_Position, Lineup_Position, Event_Type, Batter_Event_Flag, AB_Flag, Hit_Value, SH_Flag, SF_Flag, Outs_on_Play, Double_Play_Flag, Triple_Play_Flag, RBI_On_Play, Wild_Pitch_Flag, Passed_Ball_Flag, Fielded_By, Batted_Ball_Type, Bunt_Flag, Foul_Flag, Hit_Location, Num_Errors, Batter_Dest, Play_on_Batter, New_Game_Flag, End_Game_Flag, Position_of_Batter_removed_for_Pinch_Hitter) Values ('5e40d66476fc4d5d594c7d226aab04e95a91eb736c7576f4eebead92', 'TEX201504290', 1, 0, 0, 0, 0, 'X', 0, 0, 'michael', 'R', 'sdfgh', 'L', 'S8/G', 'T', 'F', 4, 1, 20, 'T', 'T', 1, 'F', 'F', 0, 'F', 'F', 0, 'F', 'F', 8, 'G', 'F', 'F', '', 0, 1, '', 'T', 'F', 0);
-
-#select * from game_day;
-#select count(*) from player_information;
-#select count(*) from event_instance;
-#select count(*) from event_instance;
 #SET SQL_SAFE_UPDATES = 0;
 #select count(*) from event_instance
 #select count(*) from error_information
 #select * from event_catcher
 #select * from event_instance
 #select game_id from game_day where date > '2019-08-29';
-#select count(*) from pitching_features;
+#select count(*) from pitching_features;    
 
-select Distinct game_day.Game_ID, batter_in_event.Batting_Team, pitcher_in_event.Pitcher_Name,       # Home Team equals One for Batting Team
-event_catcher.Catcher, event_centre_field.Center_Field, event_left_field.Left_Field,
-event_right_field.Right_Field, event_first_base.First_Base, event_second_base.Second_Base, 
-event_third_base.Third_Base, event_shortstop.Shortstop
-    from game_day
-	inner join event_instance on game_day.Game_ID=event_instance.Game_ID
-	inner join batter_in_event on event_instance.idEvent=batter_in_event.idEvent
-    inner join pitcher_in_event on pitcher_in_event.idEvent=event_instance.idEvent
-    inner join event_catcher on event_catcher.idEvent=event_instance.idEvent
-    inner join event_centre_field on event_centre_field.idEvent=event_instance.idEvent
-    inner join event_left_field on event_left_field.idEvent=event_instance.idEvent
-    inner join event_right_field on event_right_field.idEvent=event_instance.idEvent
-    inner join event_first_base on event_first_base.idEvent=event_instance.idEvent
-    inner join event_second_base on event_second_base.idEvent=event_instance.idEvent
-    inner join event_third_base on event_third_base.idEvent=event_instance.idEvent
-    inner join event_shortstop on event_shortstop.idEvent=event_instance.idEvent
-    where event_instance.Game_ID = 'ANA200604070'
-    order by batter_in_event.batting_team, CHAR_LENGTH(event_instance.idEvent), event_instance.idEvent;
+select * from event_instance where game_id = 'BOS199004090'
 
-select event_instance.Game_ID,
-(case when event_instance.Vis_Score > event_instance.Home_Score then 1 else 0 end) as Home_Win
-	from event_instance
-	inner join game_day on event_instance.Game_ID=game_day.Game_ID 
-    where event_instance.End_Game_Flag = 'T'
-    order by game_day.Date;
+    
+    
+    
+    
+    
     
