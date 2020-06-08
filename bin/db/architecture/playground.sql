f1c8a3a379d83634c92ed8e4eb854e6ff834bec1
michaelkrakovsky <34549116+michaelkrakovsky@users.noreply.github.com>
2020-06-07 20:18:09 -0400

    New: ROC

--- bin/db/architecture/playground.sql
+++ bin/db/architecture/playground.sql
@@ -7,10 +7,5 @@
 #select * from positional_player_details;
 #INSERT IGNORE INTO positional_player_details (player_id, Last_Name, First_Name, Player_Debut) values ('michael', 'deleteMe', 'deleteMe', '2019-04-23');
 
-select offensive_features.Game_ID, offensive_features.player_id, 
-Ten_Rolling_BA, Ten_Rolling_OBP, Ten_Rolling_SLG from offensive_features inner join
-game_day on game_day.Game_ID=offensive_features.Game_ID
-order by game_day.Date asc
-                                
-                                
-                                
+select * from offensive_features where offensive_features.Game_ID = 'LAN199007270'
+         
